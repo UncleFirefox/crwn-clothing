3c1121e6a44bd1d918209b0230c1d3513d3b7067
Albert Rodriguez <albert.rodriguez@seddi.com>
2021-09-08 19:53:34 +0200

    implemented firebase utils storing authenticated users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,29 @@ const config = {
     appId: "1:864028872048:web:e5cd47170110e97b0593b5"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    
+    if(!snapShot.exists){
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
